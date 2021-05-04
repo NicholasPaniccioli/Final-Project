8f0a22ca4e8525dde673caf9d3cacae7f7721d4d
NicholasPaniccioli <njp7745@g.rit.edu>
2021-05-04 19:26:58 -0400

    Found error, forgot to remove signupPage, now deleted

--- server/controllers/Account.js
+++ server/controllers/Account.js
@@ -6,10 +6,6 @@ const loginPage = (req, res) => {
   res.render('login', { csrfToken: req.csrfToken() });
 };
 
-const signupPage = (req, res) => {
-  res.render('signup', { csrfToken: req.csrfToken() });
-};
-
 const logout = (req, res) => {
   req.session.destroy();
   res.redirect('/');
@@ -93,7 +89,6 @@ const getToken = (request, response) => {
 };
 
 module.exports.loginPage = loginPage;
-module.exports.signupPage = signupPage;
 module.exports.login = login;
 module.exports.logout = logout;
 module.exports.signup = signup;
